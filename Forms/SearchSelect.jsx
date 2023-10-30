85e8093d8d8a6ea344add5ea55fc7ee834991234
nicolas <nicolas@cco-info.fr>
2023-10-30 15:48:27 +0100

    Modif Search select

--- Forms/SearchSelect.jsx
+++ Forms/SearchSelect.jsx
@@ -47,7 +47,16 @@ export default function SearchSelect({
             className
           }
         />
-        <i className="fi fi-ss-exclamation text-sm opacity-0 peer-invalid:opacity-100 absolute right-4 text-error-color" />
+        <Combobox.Button className="peer absolute right-0 top-0 h-full w-14 flex items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+            <path
+              fillRule="evenodd"
+              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Combobox.Button>
+        <i className="fi fi-ss-exclamation pointer-events-none text-sm opacity-0 peer-invalid:opacity-100 absolute right-1 text-error-color" />
         <Transition
           className="absolute w-full left-2 top-12 z-30"
           enter="transition duration-100 ease-out"
