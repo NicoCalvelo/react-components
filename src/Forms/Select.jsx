b4b5bc9e96459ce553fce727fa9655bd6025ad9f
Nicolas <nicolas@cco-info.fr>
2025-02-17 11:18:24 +0100

    modifs selecet

--- src/Forms/Select.jsx
+++ src/Forms/Select.jsx
@@ -62,47 +62,51 @@ export default function Select({
             {title}
           </label>
         )}
-        {loading && <Spinner className="w-4 h-4 absolute right-8" />}
-        {!loading && (
-          <ListboxButton
-            as="div"
-            className={
-              buttonClassName +
-              " peer flex items-center input !w-full max-w-full flex-shrink-0 border border-gray-300 rounded-lg pl-2 pr-4 " +
-              (title ? "pt-3 pb-2.5" : "py-1.5")
-            }
-          >
-            {selected && (!isMulti || (isMulti && selected?.length > 0)) && (
-              <p className="font-medium truncate flex-grow">
-                {isMulti ? selected.map((item) => item.label ?? item.name).join(", ") : selected.label ?? selected.name}
-              </p>
-            )}
-            {(selected === null || (isMulti && selected?.length === 0)) && <p className="text-text-light flex-grow truncate font-light">{placeholder}</p>}
-            {isNullable && ((!isMulti && selected) || (isMulti && selected?.length > 0)) && (
-              <svg
-                onClick={() => {
-                  setSelected(isMulti ? [] : null);
-                  if (onChange) onChange(isMulti ? [] : null);
-                }}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-4 h-4 cursor-pointer flex-shrink-0"
-              >
-                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-              </svg>
-            )}
-            {(!selected || (isMulti && selected.length === 0) || !isNullable) && (
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 flex-shrink-0">
-                <path
-                  fillRule="evenodd"
-                  d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            )}
-          </ListboxButton>
-        )}
+        <ListboxButton
+          as="div"
+          className={
+            buttonClassName +
+            " peer flex items-center placeholder:italic placeholder:text-gray-300 input !w-full max-w-full flex-shrink-0 border border-gray-300 rounded-lg pl-2 pr-4 " +
+            (title ? "pt-3 pb-2.5" : "py-1.5") +
+            " " +
+            (loading ? " min-h-10" : "")
+          }
+        >
+          {loading && <Spinner className="w-4 h-4 absolute right-8" />}
+          {!loading && (
+            <>
+              {selected && (!isMulti || (isMulti && selected?.length > 0)) && (
+                <p className="font-medium truncate flex-grow">
+                  {isMulti ? selected.map((item) => item.label ?? item.name).join(", ") : selected.label ?? selected.name}
+                </p>
+              )}
+              {(selected === null || (isMulti && selected?.length === 0)) && <p className="text-text-light flex-grow truncate font-light">{placeholder}</p>}
+              {isNullable && ((!isMulti && selected) || (isMulti && selected?.length > 0)) && (
+                <svg
+                  onClick={() => {
+                    setSelected(isMulti ? [] : null);
+                    if (onChange) onChange(isMulti ? [] : null);
+                  }}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-4 h-4 cursor-pointer flex-shrink-0"
+                >
+                  <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                </svg>
+              )}
+              {(!selected || (isMulti && selected.length === 0) || !isNullable) && (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 flex-shrink-0">
+                  <path
+                    fillRule="evenodd"
+                    d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              )}
+            </>
+          )}
+        </ListboxButton>
         {!loading && (
           <Transition
             className="absolute w-fit right-0 top-12 z-30"
